0f27ee2cefa5c03faac5a461cf016ca6eca85b6b
vijayannamalai <vijayannamalai55@gmail.com>
2024-08-09 22:20:00 +0530

    portfolio_desktop version

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -1,56 +1,58 @@
-import React from 'react'
-import { Element } from 'react-scroll'
-import Project from '../Project/Project'
-import './ProjectContainer.css'
+import React, { useState } from 'react';
+import { Element } from 'react-scroll';
+import Project from '../Project/Project';
+import './ProjectContainer.css';
+import cartapp from '../../assets/cartapp.png';
+import responsivesite from  '../../assets/responsivesite.png';
+import quizapp from '../../assets/quizapp.png';
+import productapp from '../../assets/productapp.png';
+import searchapp from '../../assets/searchapp.png';
+import tributeapp from '../../assets/tributeapp.png';
+
 const ProjectContainer = () => {
+    const [activeIndex, setActiveIndex] = useState(null);
 
-    const projects =[
-        {img:'https://i.pinimg.com/originals/bf/82/f6/bf82f6956a32819af48c2572243e8286.jpg',
-            title:'Facebook',
-            desc:'Facebook is the good social media plateform',
-            link:'www.facebook.com'
-        },
-        {img:'https://i.pinimg.com/originals/bf/82/f6/bf82f6956a32819af48c2572243e8286.jpg',
-            title:'Facebook',
-            desc:'Facebook is the good social media plateform',
-            link:'www.facebook.com'
-        },
-        {img:'https://i.pinimg.com/originals/bf/82/f6/bf82f6956a32819af48c2572243e8286.jpg',
-            title:'Facebook',
-            desc:'Facebook is the good social media plateform',
-            link:'www.facebook.com'
-        },
-        {img:'https://i.pinimg.com/originals/bf/82/f6/bf82f6956a32819af48c2572243e8286.jpg',
-            title:'Facebook',
-            desc:'Facebook is the good social media plateform',
-            link:'www.facebook.com'
-        },
-        {img:'https://i.pinimg.com/originals/bf/82/f6/bf82f6956a32819af48c2572243e8286.jpg',
-            title:'Facebook',
-            desc:'Facebook is the good social media plateform',
-            link:'www.facebook.com'
-        },
-        {img:'https://i.pinimg.com/originals/bf/82/f6/bf82f6956a32819af48c2572243e8286.jpg',
-            title:'Facebook',
-            desc:'Facebook is the good social media plateform',
-            link:'www.facebook.com'
-        }
-    ]
-  return (
-    <Element className='projectContainer' name='projects'>
-     <h1>Projects</h1>
-     <p>Here are some projects which I done for making lives of people easy</p>
-     <div className='projectContainer__projects'>
-     {
-        projects.map((project,index)=>{
-            return (
-                <Project key={index} img={project.img} title={project.title} desc={project.desc} link={project.link}/>
-            )
-        })
-     }
-     </div>
-    </Element>
-  )
-}
+    const projects = [
+        { img: cartapp, title: 'Shopping Cart App', 
+            desc: 'This app is used to calculate the total amount for ordered products. Developed using a dummy API.', 
+            link: 'https://www.vijayannamalai.com/cartapp' },
+        { img: productapp, title: 'Product App', 
+            desc: 'An app for CRUD operations on products. It is a full-stack MERN app with image uploading functionalities.', 
+            link: 'https://www.vijayannamalai.com/product_app' },
+        { img: searchapp, title: 'Employee App', 
+            desc: 'An app to display employee details, including search functionalities. Built using React.', 
+            link: 'https://www.vijayannamalai.com/searching_app' },
+        { img: tributeapp, title: 'Tribute Page', 
+            desc: 'A tribute webpage dedicated to the Late A.P.J. Abdul Kalam Sir. A simple responsive site built with Bootstrap.', 
+            link: 'https://www.vijayannamalai.com/web' },
+        { img: quizapp, title: 'Simple Quiz App', 
+            desc: 'An app where users answer questions and receive their scores. Built using HTML, CSS, and JavaScript.', 
+            link: 'https://www.vijayannamalai.com/quizapp' },
+        { img: responsivesite, title: 'Food Website', 
+            desc: 'A static food website with a responsive design, created without using Bootstrap. Built with HTML, CSS, and JavaScript.', 
+            link: 'https://www.vijayannamalai.com/responsivewebsite' }
+    ];
 
-export default ProjectContainer
+    return (
+        <Element className="projectContainer" name="projects">
+            <h1>Projects</h1>
+            <p>Here are some projects which I have done</p>
+            <div className="projectContainer__projects">
+                {projects.map((project, index) => (
+                    <Project
+                        key={index}
+                        img={project.img}
+                        title={project.title}
+                        desc={project.desc}
+                        link={project.link}
+                        isActive={activeIndex === index}
+                        onMouseEnter={() => setActiveIndex(index)}
+                        onMouseLeave={() => setActiveIndex(null)}
+                    />
+                ))}
+            </div>
+        </Element>
+    );
+};
+
+export default ProjectContainer;
--- src/components/TopContent/TopContent.js
+++ src/components/TopContent/TopContent.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-scroll'
 import './TopContent.css'
+import resume from '../../assets/resume.pdf'
 const TopContent = () => {
     return (
         <div className='topContent'>
             <div className='topContent__container'>
                 <h1>Mr.Annamalai</h1>
                 <p>A Professional Web and App Developer</p>
-                <a href="www.google.com">
+                <a href={resume} download="annamalai_resume.pdf">
                     <button className='topContent__downloadButton'>Download CV</button></a>
                     <Link to="projects" smooth={true} duration={500}>
                     <button className='topContent__workButton'>My Work</button>
